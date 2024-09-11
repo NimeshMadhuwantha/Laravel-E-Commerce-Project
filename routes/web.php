ac38cdce7dd34b20ade1d8caf8462319502cd9c3
NimeshMadhuwantha <janmadhuwantha@std.appsc.sab.ac.lk>
2024-09-11 12:03:39 +0530

    create admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController; 
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,3 +16,5 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+route::get('redirect',[HomeController::class,'redirect']);
