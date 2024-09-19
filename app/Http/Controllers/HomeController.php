286b4a48ea85d5f74391616c36bd26e2725b392d
NimeshMadhuwantha <janmadhuwantha@std.appsc.sab.ac.lk>
2024-09-19 13:59:12 +0530

    create navbar and adjest

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -21,7 +21,7 @@ class HomeController extends Controller
             return view('admin.home');
         }
         else{
-            return view('dashboard');
+            return view('Home.userpage');
         }
     }
 }
